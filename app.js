9d94484a8a70c932a411d546ec2d864656035f14
KBJPROG <rqw0618@naver.com>
2022-06-22 19:02:51 +0900

    session으로 month정보저장 구현

--- app.js
+++ app.js
@@ -2,14 +2,40 @@ const express = require('express')
 const app = new express()
 const path = require('path')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
+const storeTodoController = require('./controllers/storeTodo')
+const openEditController = require('./controllers/openEdit')
+const expressSession = require('express-session')
+
+mongoose.connect('mongodb://localhost/my_calender', {useNewUrlParser:true});
+
+global.chosenDate = null;
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(expressSession({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use('/edit', (req, res, next)=>{
+    chosenDate = req.session.chosenDate;
+    next();
+})
+
+app.get('/edit',(req,res)=>{
+    res.render('edit');
+})
 
 app.get('/', (req, res)=>{
     res.render('index');
 });
+
+app.post('/openedit', openEditController)
+app.post('/todo/store', storeTodoController)
 
 app.listen(8000, ()=>{
     console.log('Server running at http://127.0.0.1:8000');     
